dbb7de47ff9c436839d002b3cf4055ffabb51ee4
Murilo Ijanc <murilo@lab804.com.br>
2016-10-26 19:52:50 +0100

    refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,53 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from gevent import monkey
-from flask import Flask
-from flask_socketio import SocketIO
-from config import config
-import paho.mqtt.client as mqtt
-
 monkey.patch_all()
 
-socketio = SocketIO()
-client = mqtt.Client()
+from flask import Flask
+
+from config import config
+from external import client, socketio
 
 
-def create_app(config_stage):
+def create_app(config_stage='default'):
     app = Flask(__name__)
+
+    # read config
     app.config.from_object(config[config_stage])
 
+    # blueprints of app
+    blueprints(app)
+
+    # external libs
+    external_lib(app)
+
+    return app
+
+
+def blueprints(app):
     from .main import main as main_blueprint
-    from .main import on_mesage
 
     app.register_blueprint(main_blueprint)
 
-    config[config_stage].init_app(app)
-    client.username_pw_set(username=config[config_stage].MQTT_USERNAME,
-                           password=config[config_stage].MQTT_PASSWORD)
-    client.connect(config[config_stage].MQTT_BROKER_URL,
-                   config[config_stage].MQTT_PORT,
-                   config[config_stage].MQTT_KEEP_ALIVE)
-    client.subscribe(config[config_stage].MQTT_TOPIC, config[config_stage].MQTT_QOS)
+
+def external_lib(app):
+    """load external lib"""
+
+    from .main import on_mesage
+
+    # MQTT
+    # set username and password
+    client.username_pw_set(username=app.config['MQTT_USERNAME'],
+                           password=app.config['MQTT_PASSWORD'])
+
+    # connecting...
+    client.connect(app.config['MQTT_BROKER_URL'],
+                   app.config['MQTT_PORT'],
+                   app.config['MQTT_KEEP_ALIVE'])
+
+    # sub
+    client.subscribe(app.config['MQTT_TOPIC'],
+                     app.config['MQTT_QOS'])
+
     client.on_message = on_mesage
 
+    # socketio
     socketio.init_app(app)
-
-    return app
--- config.py
+++ config.py
@@ -7,8 +7,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    # base config
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'testing'
+
+    # mqtt config
     MQTT_USERNAME = os.environ.get('MQTT_SERVER_USERNAME') or "server_listener"
     MQTT_PASSWORD = os.environ.get('MQTT_PASSWORD') or "l4b804"
     MQTT_BROKER_URL = "0.0.0.0"
@@ -16,11 +19,9 @@ class Config:
     MQTT_KEEP_ALIVE = 60
     MQTT_TOPIC = "weather_data"
     MQTT_QOS = 0
-    NOTIFICATIONKEY = os.environ.get('NOTIFICATIONKEY') or None
 
-    @staticmethod
-    def init_app(app):
-        pass
+    # notification app mobile
+    NOTIFICATIONKEY = os.environ.get('NOTIFICATIONKEY') or None
 
 
 config = {'default': Config}
