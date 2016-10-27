9724ceb0f444e84b39827b50d7c9aff2542c99db
Murilo Ijanc <murilo@lab804.com.br>
2016-10-27 15:33:11 +0100

    pep8

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from flask import render_template
 
 from . import main
 
+
 @main.route('/')
 def index():
-
     return render_template('index.html')
