6825455eceb0f5ecdc82092b7ca96290198760ff
joaoTrevizoli <joao@lab804.com.br>
2016-10-28 03:20:49 +0100

    Added the aquacrop class to the file external.py and events updated to use the new aquacrop class

--- app/external.py
+++ app/external.py
@@ -1,5 +1,16 @@
 # import paho.mqtt.client as mqtt
 from flask_socketio import SocketIO
+from labmet.fao_aquacrop_model.prodfao import AquaCropModel
 
+aquacrop_data = {"culture_name": "potato",
+                 "ky": 1.1,
+                 "lat": 51.5044968,
+                 "eto_culture": 0.8,
+                 "avg_year_temp": 19,
+                 "n_days": 130,
+                 "peak_l_a_index": 3,
+                 "awc": 35}
+
+aqua_crop_model = AquaCropModel(**aquacrop_data)
 socketio = SocketIO()
 # client = mqtt.Client()
--- app/main/events.py
+++ app/main/events.py
@@ -7,32 +7,9 @@ from .. import socketio
 
 from config import Config
 from notification import Notification
-
-from labmet import ExtraterrestrialIrradiance, \
-    ThornthwaiteETo, ObtainableProductivity, \
-    SummerTemperatureFixCIII, PotentialProductivity, BreathingFix, \
-    HarvestedPartFix, LeafAreaIndexFix, lux_to_n_N, soil_moisture_to_mm
+from app.external import aqua_crop_model
 
 notification = Notification(Config.NOTIFICATIONKEY)  # o.0
-
-eto_potato = 0.8
-awc_potato = 35
-soil_moisture = 35
-precipitation = 0
-ky_potato = 1.1
-avg_year_temp = 19
-radiation_data = {
-        "day": datetime.now(),
-        "lat": 51.5044968
-    }
-extra_radiation = ExtraterrestrialIrradiance(**radiation_data)
-eto = ThornthwaiteETo(avg_year_temp,
-                      extra_radiation.photoperiod(),
-                      1,
-                      avg_year_temp).eto_day() * eto_potato
-real_et = eto
-
-obtainable_productivity = ObtainableProductivity(ky=ky_potato)
 
 NIGHT = 5  # Hour (supposedly)
 SECONDS_DELAY = 10 # Delay send push just example massive.
@@ -55,73 +32,20 @@ CULTURE = {
 def on_mesage(mosq, obj, msg):
     global ERROR  # crazy o.0
     global DELAY
-    global obtainable_productivity
-    global extra_radiation
-    global soil_moisture
-    global precipitation
-    global eto
-    global awc_potato
-    global real_et
 
-    n_days = 130
-    peak_l_a_index = 3
     json_msg = json.loads(msg.payload)
-    n_N = lux_to_n_N(json_msg["bh1750_illuminance"])
     json_msg['id'] = 123
     json_msg['status'] = 1
 
-    radiation_data = {
-        "day": datetime.now(),
-        "lat": 51.5044968
-    }
-    extra_radiation = ExtraterrestrialIrradiance(**radiation_data)
+    productivity_values_data = {"soil_moisture": json_msg["analog_soil_moisture"],
+                                "temperature": json_msg["ds18b20_temp"],
+                                "illuminance": json_msg["bh1750_illuminance"],
+                                "date": datetime.now()
+                                }
 
-    temp_cloudy_days_fix = SummerTemperatureFixCIII(temperature=json_msg["ds18b20_temp"]).clear_days_fix()
-    temp_clear_days_fix = SummerTemperatureFixCIII(temperature=json_msg["ds18b20_temp"]).cloudy_days_fix()
+    productivity_values = aqua_crop_model.aqua_crop(**productivity_values_data)
 
-    potential_productivity = PotentialProductivity(extra_radiation.ho_cal_sqaured_cm(),
-                                                   temp_cloudy_days_fix=temp_cloudy_days_fix,
-                                                   temp_clear_days_fix=temp_clear_days_fix,
-                                                   n_N=n_N)
-
-    breath_fix = BreathingFix(temperature=json_msg["ds18b20_temp"]).breathing_fix()
-    harvest_fix = HarvestedPartFix("potato").harvested_part_fix()["average"]
-    leaf_area_fix = LeafAreaIndexFix(peak_l_a_index).leaf_area_index_fix()
-
-    json_msg["potential_productivity"] = potential_productivity.potential_productivity(leaf_area_fix,
-                                                                                       breath_fix,
-                                                                                       harvest_fix,
-                                                                                       n_days,
-                                                                                       hectometer_sqr_m=True)
-    json_msg["real_productivity"] = \
-        obtainable_productivity.obtainable_productivity(etc=real_et,
-                                                        eto=eto,
-                                                        potential_productivity=json_msg["potential_productivity"])
-
-    read_soil_moisture = soil_moisture_to_mm(json_msg['analog_soil_moisture'], awc_potato)
-    variation = read_soil_moisture - soil_moisture
-
-    if read_soil_moisture > eto:
-        real_et = eto
-        if variation >= awc_potato:
-            precipitation = awc_potato
-        else:
-            precipitation = variation
-    else:
-        precipitation = 0
-        real_et = read_soil_moisture
-
-    soil_moisture = read_soil_moisture
-    radiation_data["day"] = datetime.now()
-    eto = ThornthwaiteETo(avg_year_temp,
-                          extra_radiation.photoperiod(),
-                          30,
-                          avg_year_temp).eto_day() * eto_potato
-
-
-    json_msg["precipitation"] = precipitation
-    json_msg["eto"] = eto
-    json_msg["etc"] = real_et
+    json_msg.update(productivity_values)
 
     socketio.emit('my response', {'topic': msg.topic, 'payload': json_msg},
                   namespace='/weather_data')
