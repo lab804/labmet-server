b9d39b75dbf081fa32f0535a2d2f8c26b96d6e90
Murilo Ijanc <murilo@lab804.com.br>
2016-10-28 06:57:17 +0100

    7AM no futher

--- app/main/events.py
+++ app/main/events.py
@@ -1,137 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import json
-from datetime import datetime, timedelta
-from .. import socketio
 
 from config import Config
-from notification import Notification
+from app.external import socketio
+from app.main.notification import Notification
 
-from labmet import ExtraterrestrialIrradiance, \
-    ThornthwaiteETo, ObtainableProductivity, \
-    SummerTemperatureFixCIII, PotentialProductivity, BreathingFix, \
-    HarvestedPartFix, LeafAreaIndexFix, lux_to_n_N, soil_moisture_to_mm
 
 notification = Notification(Config.NOTIFICATIONKEY)  # o.0
 
-eto_potato = 0.8
-awc_potato = 35
-soil_moisture = 35
-precipitation = 0
-ky_potato = 1.1
-avg_year_temp = 19
-radiation_data = {
-        "day": datetime.now(),
-        "lat": 51.5044968
-    }
-extra_radiation = ExtraterrestrialIrradiance(**radiation_data)
-eto = ThornthwaiteETo(avg_year_temp,
-                      extra_radiation.photoperiod(),
-                      1,
-                      avg_year_temp).eto_day() * eto_potato
-real_et = eto
 
-obtainable_productivity = ObtainableProductivity(ky=ky_potato)
-
-NIGHT = 5  # Hour (supposedly)
-SECONDS_DELAY = 10 # Delay send push just example massive.
-DELAY = datetime.now() + timedelta(seconds=SECONDS_DELAY)
-
-CULTURE = {
-    'POTATO': {
-        'BEST_PERFOMACE': {
-            'ILLUMINACE': 3,
-            'TEMPMAX': 30.5,
-            'TEMPMIN': 9.3,
-        },
-        'MSGS': {
-            'LESS_ILLUMINACE': 'missing light'
-        }
-    }
-}
-
-
-def on_mesage(mosq, obj, msg):
-    global ERROR  # crazy o.0
-    global DELAY
-    global obtainable_productivity
-    global extra_radiation
-    global soil_moisture
-    global precipitation
-    global eto
-    global awc_potato
-    global real_et
-
-    n_days = 130
-    peak_l_a_index = 3
-    json_msg = json.loads(msg.payload)
-    n_N = lux_to_n_N(json_msg["bh1750_illuminance"])
-    json_msg['id'] = 123
-    json_msg['status'] = 1
-
-    radiation_data = {
-        "day": datetime.now(),
-        "lat": 51.5044968
-    }
-    extra_radiation = ExtraterrestrialIrradiance(**radiation_data)
-
-    temp_cloudy_days_fix = SummerTemperatureFixCIII(temperature=json_msg["ds18b20_temp"]).clear_days_fix()
-    temp_clear_days_fix = SummerTemperatureFixCIII(temperature=json_msg["ds18b20_temp"]).cloudy_days_fix()
-
-    potential_productivity = PotentialProductivity(extra_radiation.ho_cal_sqaured_cm(),
-                                                   temp_cloudy_days_fix=temp_cloudy_days_fix,
-                                                   temp_clear_days_fix=temp_clear_days_fix,
-                                                   n_N=n_N)
-
-    breath_fix = BreathingFix(temperature=json_msg["ds18b20_temp"]).breathing_fix()
-    harvest_fix = HarvestedPartFix("potato").harvested_part_fix()["average"]
-    leaf_area_fix = LeafAreaIndexFix(peak_l_a_index).leaf_area_index_fix()
-
-    json_msg["potential_productivity"] = potential_productivity.potential_productivity(leaf_area_fix,
-                                                                                       breath_fix,
-                                                                                       harvest_fix,
-                                                                                       n_days,
-                                                                                       hectometer_sqr_m=True)
-    json_msg["real_productivity"] = \
-        obtainable_productivity.obtainable_productivity(etc=real_et,
-                                                        eto=eto,
-                                                        potential_productivity=json_msg["potential_productivity"])
-
-    read_soil_moisture = soil_moisture_to_mm(json_msg['analog_soil_moisture'], awc_potato)
-    variation = read_soil_moisture - soil_moisture
-
-    if read_soil_moisture > eto:
-        real_et = eto
-        if variation >= awc_potato:
-            precipitation = awc_potato
-        else:
-            precipitation = variation
-    else:
-        precipitation = 0
-        real_et = read_soil_moisture
-
-    soil_moisture = read_soil_moisture
-    radiation_data["day"] = datetime.now()
-    eto = ThornthwaiteETo(avg_year_temp,
-                          extra_radiation.photoperiod(),
-                          30,
-                          avg_year_temp).eto_day() * eto_potato
-
-
-    json_msg["precipitation"] = precipitation
-    json_msg["eto"] = eto
-    json_msg["etc"] = real_et
-
-    socketio.emit('my response', {'topic': msg.topic, 'payload': json_msg},
+@socketio.on('stations')
+def station_receive(json_data):
+    socketio.emit('station_data', json.loads(json_data),
                   namespace='/weather_data')
-
-    if json_msg['bh1750_illuminance'] < CULTURE['POTATO']['BEST_PERFOMACE']['ILLUMINACE']:
-        # is night?
-        now = datetime.now()
-
-        if now.hour > NIGHT and now > DELAY:
-            msg = CULTURE['POTATO']['MSGS']['LESS_ILLUMINACE']
-            notification.send_push_all(msg)
-            ERROR = True
-            DELAY = now + timedelta(seconds=SECONDS_DELAY)
---
+++ app/static/js/labmet-jquery.js
@@ -0,0 +1,311 @@
+/**
+ * Created by joao on 17/05/16.
+ */
+
+(function($) {
+    $.fn.extend({
+        'LabMet': function(_options) {
+            var _defaulthost = 'http://' + document.domain + ':' + location.port;
+
+            var showMsgDisc = false,
+                shoMsgConn = true,
+                graphicsData = {},
+                lastIndex = 1; // simple example plot x
+
+            var options = $.extend({
+                'host': _defaulthost,
+                '_id': [],
+                'namespace': 'weather_data',
+            }, _options);
+
+            var _ids = options['_id'];
+
+            var _host = options['host'] + '/' + options['namespace'];
+
+            var socket = io.connect(_host);
+
+            /*
+            plot
+            */
+            var PlotData = function(_id) {
+              $.plot($("#flot-"+_id), [
+                  graphicsData[_id]['data_obt'], graphicsData[_id]['data_pot']
+              ], {
+                  series: {
+                      lines: {
+                          show: true,
+                          fill: true
+                      },
+                      splines: {
+                          show: true,
+                          tension: 0.4,
+                          lineWidth: 1,
+                          fill: 0.4
+                      },
+                      points: {
+                          radius: 0,
+                          show: true
+                      },
+                      shadowSize: 2
+                  },
+                  grid: {
+                      hoverable: true,
+                      clickable: true,
+                      tickColor: "#d5d5d5",
+                      borderWidth: 1,
+                      color: '#d5d5d5'
+                  },
+                  colors: ["#1ab394", "#1C84C6"],
+                  xaxis: {},
+                  yaxis: {
+                      ticks: 4
+                  },
+                  tooltip: false
+              });
+            };
+
+
+            if (_ids.length > 0 && _ids.length <= 10) {
+
+              for (var i = 0; i < _ids.length; i++) {
+                var _id = _ids[i];
+
+                // creating for populate data
+                graphicsData[_id] = {'data_obt': [], 'data_pot':[]};
+
+                var card = ['<div class="browser-mockup" style="background-color: #fff; margin-top: 25px;">',
+                '    <div><img class="ico-labmet" src="/static/img/icon.png" /></div>',
+                '    <div class="status status-disconnected"><div>',
+                '    <div class="row" style="padding: 20px;">',
+                '      <div class="col-md-3">',
+                '          <h2 style="margin-top: 10px; font-size: 26px;">My Station <small id="id-'+_id+'" class=""></small></h2>',
+                '          <div>last update: <small id="collected_at-'+_id+'" style="font-size: 85%"></small></div>',
+                '          <ul class="list-group clear-list" style="margin-top: 15px;">',
+                '              <li class="list-group-item fist-item" style="padding: 10px 0; border-top: 0; border-left:0; border-right: 0; border-bottom: 0;">',
+                '                  <span class="pull-right animated" id="bmp180_temp-'+_id+'">',
+                '                      0',
+                '                  </span>',
+                '                  <span class="label label-success" style="margin-right: 10px;">OK</span> Temperature',
+                '              </li>',
+                '              <li class="list-group-item"  style="padding: 10px 0; border-top: 1px solid #e7eaec; border-left:0; border-right: 0; border-bottom: 0;">',
+                '                  <span class="pull-right animated" id="bmp180_alt-'+_id+'">',
+                '                      0',
+                '                  </span>',
+                '                  <span class="label label-success" style="margin-right: 10px;">OK</span> Altitude',
+                '              </li>',
+                '              <li class="list-group-item" style="padding: 10px 0; border-top: 1px solid #e7eaec; border-left:0; border-right: 0; border-bottom: 0;">',
+                '                  <span class="pull-right animated" id="bmp180_press-'+_id+'">',
+                '                      0',
+                '                  </span>',
+                '                  <span class="label label-success" style="margin-right: 10px;">OK</span> Pression',
+                '              </li>',
+                '              <li class="list-group-item" style="padding: 10px 0; border-top: 1px solid #e7eaec; border-left:0; border-right: 0; border-bottom: 0;">',
+                '                  <span class="pull-right animated" id="dht22_humid-'+_id+'">',
+                '                      0',
+                '                  </span>',
+                '                  <span class="label label-success" style="margin-right: 10px;">OK</span> Humidade',
+                '              </li>',
+                '              <li class="list-group-item" style="padding: 10px 0; border-top: 1px solid #e7eaec; border-left:0; border-right: 0; border-bottom: 0;">',
+                '                  <span class="pull-right animated" id="bh1750_illuminance-'+_id+'">',
+                '                      0',
+                '                  </span>',
+                '                  <span class="label label-success" style="margin-right: 10px;">OK</span> Iluminance',
+                '              </li>',
+                '              <li class="list-group-item" style="padding: 10px 0; border-top: 1px solid #e7eaec; border-left:0; border-right: 0; border-bottom: 0;">',
+                '                  <span class="pull-right animated" id="analog_soil_moisture-'+_id+'">',
+                '                      0',
+                '                  </span>',
+                '                  <span class="label label-success" style="margin-right: 10px;">OK</span> Soil Moisture',
+                '              </li>',
+                '            </ul>',
+                '    </div>',
+                '    <div class="col-md-6">',
+                '      <div class="flot-chart dashboard-chart">',
+                '          <div class="labmet-flot" id="flot-'+_id+'"></div>',
+                '          <div class="row text-left">',
+                '            <div class="col-xs-4">',
+                '              <div style="margin-left: 20px;">',
+                '                <span class="h4 font-bold" style="display: block !important; margin-top: 15px; font-weight: 600;" id="potential_productivity-'+_id+'">0</span>',
+                '                <small class="text-muted" style="display: block !important; margin-bottom: 15px; color: #888888;">Real productivity</small>',
+                '              </div>',
+                '            </div>',
+                '            <div class="col-xs-4">',
+                '              <div style="margin-left: 20px;">',
+                '                <span class="h4 font-bold" style="display: block !important;margin-top: 15px; font-weight: 600;" id="etc-'+_id+'">0</span>',
+                '                <small class="text-muted" style="display: block !important;margin-bottom: 15px; color: #888888;">Quanto está consumindo</small>',
+                '              </div>',
+                '            </div>',
+                '            <div class="col-xs-4">',
+                '              <div style="margin-left: 20px;">',
+                '                <span class="h4 font-bold" style="display: block !important;margin-top: 15px; font-weight: 600;" id="eto-'+_id+'">0</span>',
+                '                <small class="text-muted" style="display: block !important;margin-bottom: 15px; color: #888888;">Quanto Deveria Consumir</small>',
+                '              </div>',
+                '            </div>',
+                '          </div>',
+                '    </div>',
+                '  </div>',
+                '  <div class="col-md-3 text-center potato">',
+                '    <img src="/static/img/potato.png" id="potato-'+_id+'" class="animated fadeInUp potato-15">',
+                '  </div>',
+                '</div>'].join("");
+
+                $(this).append(card);
+
+                PlotData(_id);
+              }
+            }
+
+
+            /*
+            function set unit TODO: "Sorry portuguese; change this!!"
+
+            - Capacidade de água no solo(*mm*)
+            - Temperatura do Ar (*°C*)
+            - Temperatura do Solo (*°C*)
+            - Pressão(*Bar*)
+            - Altitude (*m*)
+            - Luminosidade (*lx* ou *lux*)
+            - Umidade Relativa (*%*)
+            - Produtividade (*Kg/m²*)
+
+            */
+            var setUnit = function(key) {
+                if (key === 'bmp180_press') {
+                    return "Bar";
+                } else if (key === 'ds18b20_temp') {
+                    return "°C*";
+                } else if (key === 'dht22_humid') {
+                    return "%"
+                } else if (key === 'analog_soil_moisture') {
+                    return "mm";
+                } else if (key === 'bmp180_temp') {
+                    return "°C";
+                } else if (key === 'bh1750_illuminance') {
+                    return "lux";
+                } else if (key === 'bmp180_alt') {
+                    return "m";
+                } else {
+                    return "";
+                }
+            };
+
+            /*
+            toPerce
+            */
+            var toPerce = function(value) {
+              var max = 4000; // change this depend culture
+              if (value > 0 || value <= 4000) {
+                div = value / 60;
+                return parseInt(div);
+              } else if (value === 4000) {
+                return 100;
+              }
+            }
+
+            /*
+            function born
+            */
+            var born = function(_id, life) {
+                _intlife = parseInt(life);
+                if (_intlife) {
+                    // remove last class
+                    $('img#potato-' + _id).removeClass('potato-*');
+                    $('img#potato-' + _id).removeClass('fadeInUp');
+                    if (_intlife > 0 && _intlife <= 15) {
+                        $('img#potato-' + _id).addClass('fadeInUp');
+                        $('img#potato-' + _id).addClass('potato-15');
+                    } else if (_intlife > 15 && _intlife <= 25) {
+                        $('img#potato-' + _id).addClass('fadeInUp');
+                        $('img#potato-' + _id).addClass('potato-25');
+                    } else if (_intlife > 25 && _intlife <= 50) {
+                        $('img#potato-' + _id).addClass('potato-50');
+                        $('img#potato-' + _id).addClass('fadeInUp');
+                    } else if (_intlife > 50 && _intlife <= 75) {
+                        $('img#potato-' + _id).addClass('potato-75');
+                        $('img#potato-' + _id).addClass('fadeInUp');
+                    } else if (_intlife > 50 && _intlife <= 100) {
+                        $('img#potato-' + _id).addClass('potato-100');
+                        $('img#potato-' + _id).addClass('fadeInUp');
+                    }
+                }
+            };
+
+            /*
+            function set data
+            */
+            var setData = function(data) {
+                // parse
+                var _data = jQuery.parseJSON(data);
+                var _id = _data['id'];
+                $.each(_data, function(key, value) {
+                    if (key === "collected_at") {
+                        $('#' + key + '-' + _id).text(value);
+                    } else if (key === "id") {
+                        $('#' + key + '-' + _id).text("#" + " " + parseInt(value).toString());
+                    } else {
+                        unit = setUnit(key);
+                        $('#' + key + '-' + _id).text(value.toFixed(2).toString() + " " + unit);
+                    }
+                });
+
+                // populate new data
+                graphicsData[_id]['data_pot'].push([lastIndex, _data['potential_productivity']]);
+                graphicsData[_id]['data_obt'].push([lastIndex, _data['obtainable_productivity']]);
+                PlotData(_id);
+                lastIndex = lastIndex + 1; // X coordinate
+
+                window.onresize = function(event) {
+                    PlotData(_id);
+                }
+
+                var potatoval = toPerce(_data['obtainable_productivity']);
+                if (potatoval >= 0 && potatoval <= 100) {
+                    born(_id, potatoval);
+                }
+
+            };
+
+
+            socket.on('connect', function() {
+                $('.status').removeClass('status-disconnected');
+                $('.status').addClass('status-connected');
+
+                if (!shoMsgConn) {
+                    $.toaster({
+                        message: 'You are online',
+                        title: 'Online',
+                        priority: 'success'
+                    });
+                    shoMsgConn = true;
+                    showMsgDisc = false;
+                }
+
+            });
+
+            socket.on('disconnect', function() {
+                $('.status').removeClass('status-connected');
+                $('.status').addClass('status-disconnected');
+
+                if (!showMsgDisc) {
+                    $.toaster({
+                        message: 'You are offline',
+                        title: 'Offline',
+                        priority: 'danger'
+                    });
+                    showMsgDisc = true;
+                    shoMsgConn = false;
+                }
+
+            });
+
+            /*
+            receive data from station
+            */
+            socket.on('station_data', function(msg) {
+                setData(msg);
+            });
+
+            return this;
+        }
+    });
+})(jQuery);
