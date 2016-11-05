3f0a6754f68b7bf592db34c83bb898b11a87efde
Murilo Ijanc <murilo@lab804.com.br>
2016-11-05 11:04:44 -0200

    don't remeber

--- worker.py
+++ worker.py
@@ -51,6 +51,7 @@ def on_message(client, userdata, msg):
     """this function send to"""
     payload = msg.payload.decode('utf-8')  # py3
     data = json.loads(payload)
+
     productivity_values_data = {"soil_moisture": data["analog_soil_moisture"],
                                 "temperature": data["ds18b20_temp"],
                                 "illuminance": data["bh1750_illuminance"],
@@ -60,7 +61,7 @@ def on_message(client, userdata, msg):
     productivity_values = aqua_crop_model.aqua_crop(**productivity_values_data)
 
     data.update(productivity_values)
-
+    print(data)
     socketIO.emit('stations', json.dumps(data))
     socketIO.wait(seconds=args['wait'])
 
